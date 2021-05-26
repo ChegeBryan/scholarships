71e6431348cdb509783045d3ec700b4a2348b1ab
Chege Bryan <cbbryan8@gmail.com>
2021-05-26 08:05:28 +0300

    Restyle app drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -6,23 +6,52 @@ class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
+      child: Column(
         children: <Widget>[
           UserAccountsDrawerHeader(
             accountName: null,
             accountEmail: Text(Provider.of<AuthProvider>(context).userEmail),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://randomuser.me/api/portraits/women/0.jpg'),
+            ),
           ),
-          ListTile(
-            title: Text('Sponsorships'),
-            onTap: () =>
-                Navigator.pushReplacementNamed(context, '/sponsorships'),
-            selected: ModalRoute.of(context).settings.name == '/sponsorships',
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.list),
+                      title: Text('Sponsorships'),
+                      onTap: () => Navigator.pushReplacementNamed(
+                          context, '/sponsorships'),
+                      selected: ModalRoute.of(context).settings.name ==
+                          '/sponsorships',
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.mode_edit),
+                      title: Text('Apply for Sponsorship'),
+                      onTap: () =>
+                          Navigator.pushReplacementNamed(context, '/apply'),
+                      selected:
+                          ModalRoute.of(context).settings.name == '/apply',
+                    )
+                  ],
+                ),
+                ListTile(
+                  leading: Icon(Icons.exit_to_app),
+                  title: Text('Logout'),
+                  onTap: () => Navigator.pushNamedAndRemoveUntil(
+                    context,
+                    '/',
+                    (Route<dynamic> route) => false,
+                  ),
+                )
+              ],
+            ),
           ),
-          ListTile(
-            title: Text('Apply for Sponsorship'),
-            onTap: () => Navigator.pushReplacementNamed(context, '/apply'),
-            selected: ModalRoute.of(context).settings.name == '/apply',
-          )
         ],
       ),
     );
