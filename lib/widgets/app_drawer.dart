39f3d545d42227715509762c97d3b0ca720c5fc4
Chege Bryan <cbbryan8@gmail.com>
2021-05-26 08:22:48 +0300

    Reduce width of drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -5,54 +5,60 @@ import 'package:provider/provider.dart';
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: null,
-            accountEmail: Text(Provider.of<AuthProvider>(context).userEmail),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://randomuser.me/api/portraits/women/0.jpg'),
+    return Container(
+      width: MediaQuery.of(context).size.width * .7,
+      child: Drawer(
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: null,
+              accountEmail: Text(Provider.of<AuthProvider>(context).userEmail),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://randomuser.me/api/portraits/women/0.jpg'),
+              ),
+              margin: const EdgeInsets.all(0.0),
             ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
+            Expanded(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    ListTile(
-                      leading: Icon(Icons.list),
-                      title: Text('Sponsorships'),
-                      onTap: () => Navigator.pushReplacementNamed(
-                          context, '/sponsorships'),
-                      selected: ModalRoute.of(context).settings.name ==
-                          '/sponsorships',
+                    Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.list),
+                          title: Text('Sponsorships'),
+                          onTap: () => Navigator.pushReplacementNamed(
+                              context, '/sponsorships'),
+                          selected: ModalRoute.of(context).settings.name ==
+                              '/sponsorships',
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.mode_edit),
+                          title: Text('Apply for Sponsorship'),
+                          onTap: () =>
+                              Navigator.pushReplacementNamed(context, '/apply'),
+                          selected:
+                              ModalRoute.of(context).settings.name == '/apply',
+                        )
+                      ],
                     ),
                     ListTile(
-                      leading: Icon(Icons.mode_edit),
-                      title: Text('Apply for Sponsorship'),
-                      onTap: () =>
-                          Navigator.pushReplacementNamed(context, '/apply'),
-                      selected:
-                          ModalRoute.of(context).settings.name == '/apply',
+                      leading: Icon(Icons.exit_to_app),
+                      title: Text('Logout'),
+                      onTap: () => Navigator.pushNamedAndRemoveUntil(
+                        context,
+                        '/',
+                        (Route<dynamic> route) => false,
+                      ),
                     )
                   ],
                 ),
-                ListTile(
-                  leading: Icon(Icons.exit_to_app),
-                  title: Text('Logout'),
-                  onTap: () => Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    '/',
-                    (Route<dynamic> route) => false,
-                  ),
-                )
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
