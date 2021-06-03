51b1ff4fe08b021f5a42d16f02d193d0ce36fe67
Chege Bryan <cbbryan8@gmail.com>
2021-06-03 15:38:19 +0300

    Remove unused imports

--- lib/register.dart
+++ lib/register.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:geopro/widgets/form_bottom_actions.dart';
 import 'package:geopro/widgets/register_form.dart';
 
