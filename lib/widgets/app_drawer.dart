582268b414172218a66d1d51987f83a92b1a80a2
Chege Bryan <cbbryan8@gmail.com>
2021-06-09 06:45:14 +0300

    Update app drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:geopro/services/user.dart';
 import 'package:geopro/util/shared_preferences.dart';
 import 'package:provider/provider.dart';
@@ -11,20 +12,62 @@ class AppDrawer extends StatelessWidget {
       child: Drawer(
         child: Column(
           children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: null,
-              accountEmail: Text(Provider.of<UserProvider>(context).user.email),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://randomuser.me/api/portraits/women/0.jpg'),
-              ),
-              margin: const EdgeInsets.all(0.0),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/app_drawer.jpg'),
-                  fit: BoxFit.cover,
+            Stack(
+              children: <Widget>[
+                ClipPath(
+                  clipper: WaveClipperOne(),
+                  child: Image.asset('assets/student_graduate.jpg'),
                 ),
-              ),
+                Positioned.fill(
+                  child: Align(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(
+                            'https://randomuser.me/api/portraits/women/0.jpg',
+                          ),
+                          radius: 48.0,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Test Name',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold,
+                                shadows: <Shadow>[
+                                  Shadow(
+                                    offset: Offset(1.0, 3.0),
+                                    blurRadius: 3.0,
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              Provider.of<UserProvider>(context).user.email,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontStyle: FontStyle.italic,
+                                shadows: <Shadow>[
+                                  Shadow(
+                                    offset: Offset(1.0, 3.0),
+                                    blurRadius: 3.0,
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
             Expanded(
               child: Container(
@@ -52,19 +95,31 @@ class AppDrawer extends StatelessWidget {
                         ),
                       ],
                     ),
-                    ListTile(
-                      leading: Icon(Icons.exit_to_app),
-                      title: Text('Logout'),
-                      onTap: () {
-                        // clear user data from local storage
-                        UserPrefences().removeUser();
-                        Navigator.pushNamedAndRemoveUntil(
-                          context,
-                          '/',
-                          (Route<dynamic> route) => false,
-                        );
-                      },
-                    )
+                    Container(
+                      width: MediaQuery.of(context).size.width * .75,
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: FlatButton(
+                        onPressed: () {
+                          // clear user data from local storage
+                          UserPrefences().removeUser();
+                          Navigator.pushNamedAndRemoveUntil(
+                            context,
+                            '/',
+                            (Route<dynamic> route) => false,
+                          );
+                        },
+                        textColor: Colors.white,
+                        shape: StadiumBorder(),
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: Text(
+                          'Logout',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                          ),
+                        ),
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
                   ],
                 ),
               ),
