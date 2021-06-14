7fc005fb7a3db187495d30807880e596b3da50e5
ChegeBryan <cbbryan8@gmail.com>
2021-06-14 10:15:59 +0300

    Fix register screen code formatting

--- lib/widgets/register_form.dart
+++ lib/widgets/register_form.dart
@@ -34,185 +34,188 @@ class _RegisterFormState extends State<RegisterForm> {
     AuthProvider auth = Provider.of<AuthProvider>(context);
 
     return Form(
-        key: _formKey,
-        autovalidate: _autovalidate,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: _username,
-              keyboardType: TextInputType.text,
-              style: TextStyle(color: Colors.white),
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.person,
-                  color: Colors.white,
-                ),
-                labelText: 'Username',
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                labelStyle: TextStyle(color: Colors.white),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
+      key: _formKey,
+      autovalidate: _autovalidate,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            controller: _username,
+            keyboardType: TextInputType.text,
+            style: TextStyle(color: Colors.white),
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.white,
               ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter username';
-                } else if (value.length < 3) {
-                  return 'Username allows a minimum of 3 characters.';
-                } else if (errors.containsKey('username')) {
-                  return errors['username'].reduce((v, e) => v + '\n' + e);
-                }
-                return null;
-              },
-              onChanged: (value) {
-                errors.remove('username');
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              controller: _email,
-              keyboardType: TextInputType.emailAddress,
-              style: TextStyle(color: Colors.white),
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.email,
-                  color: Colors.white,
-                ),
-                labelText: 'Email',
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                labelStyle: TextStyle(color: Colors.white),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
+              labelText: 'Username',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
               ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter email';
-                } else if (!isEmail(value)) {
-                  return 'Provide a valid email';
-                } else if (errors.containsKey('email')) {
-                  return errors['email'].reduce((v, e) => v + '\n' + e);
-                }
-                return null;
-              },
-              onChanged: (value) {
-                errors.remove('email');
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              controller: _password,
-              style: TextStyle(color: Colors.white),
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.lock,
-                  color: Colors.white,
-                ),
-                labelText: 'Password',
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                labelStyle: TextStyle(color: Colors.white),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
+              labelStyle: TextStyle(color: Colors.white),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
               ),
-              obscureText: true,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter password';
-                } else if (value.length < 8) {
-                  return 'Password cannot be less than 8 Characters';
-                } else if (errors.containsKey('password')) {
-                  return errors['password'].reduce((v, e) => v + '\n' + e);
-                }
-                return null;
-              },
-              onChanged: (value) {
-                errors.remove('password');
-              },
             ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              controller: _confirmPassword,
-              autocorrect: false,
-              style: TextStyle(color: Colors.white),
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.lock,
-                  color: Colors.white,
-                ),
-                labelText: 'Confirm Password',
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
-                labelStyle: TextStyle(color: Colors.white),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white),
-                ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter username';
+              } else if (value.length < 3) {
+                return 'Username allows a minimum of 3 characters.';
+              } else if (errors.containsKey('username')) {
+                return errors['username'].reduce((v, e) => v + '\n' + e);
+              }
+              return null;
+            },
+            onChanged: (value) {
+              errors.remove('username');
+            },
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          TextFormField(
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.white),
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
               ),
-              obscureText: true,
-              textAlignVertical: TextAlignVertical.center,
-              validator: (value) =>
-                  value != _password.text ? 'Password does not match' : null,
+              labelText: 'Email',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              labelStyle: TextStyle(color: Colors.white),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
             ),
-            SizedBox(
-              height: 16.0,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter email';
+              } else if (!isEmail(value)) {
+                return 'Provide a valid email';
+              } else if (errors.containsKey('email')) {
+                return errors['email'].reduce((v, e) => v + '\n' + e);
+              }
+              return null;
+            },
+            onChanged: (value) {
+              errors.remove('email');
+            },
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          TextFormField(
+            controller: _password,
+            style: TextStyle(color: Colors.white),
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              labelText: 'Password',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              labelStyle: TextStyle(color: Colors.white),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
             ),
-            auth.loggedInStatus == Status.Authenticating
-                ? CircularProgressIndicator()
-                : FlatButton(
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  _formKey.currentState.save();
+            obscureText: true,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter password';
+              } else if (value.length < 8) {
+                return 'Password cannot be less than 8 Characters';
+              } else if (errors.containsKey('password')) {
+                return errors['password'].reduce((v, e) => v + '\n' + e);
+              }
+              return null;
+            },
+            onChanged: (value) {
+              errors.remove('password');
+            },
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          TextFormField(
+            controller: _confirmPassword,
+            autocorrect: false,
+            style: TextStyle(color: Colors.white),
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.white,
+              ),
+              labelText: 'Confirm Password',
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              labelStyle: TextStyle(color: Colors.white),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+            obscureText: true,
+            textAlignVertical: TextAlignVertical.center,
+            validator: (value) =>
+                value != _password.text ? 'Password does not match' : null,
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          auth.loggedInStatus == Status.Authenticating
+              ? CircularProgressIndicator()
+              : FlatButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
 
-                  final Future<Map<String, dynamic>> successMessage = auth
-                      .register(_email.text, _username.text, _password.text);
+                      final Future<Map<String, dynamic>> successMessage =
+                          auth.register(
+                              _email.text, _username.text, _password.text);
 
-                  successMessage.then((response) {
-                    if (response['status']) {
-                      Provider.of<UserProvider>(context, listen: false)
-                          .setUser(response['user']);
-                      Navigator.pushReplacementNamed(context, '/sponsorships');
+                      successMessage.then((response) {
+                        if (response['status']) {
+                          Provider.of<UserProvider>(context, listen: false)
+                              .setUser(response['user']);
+                          Navigator.pushReplacementNamed(
+                              context, '/sponsorships');
+                        } else {
+                          Map<String, dynamic> responseErrors = {};
+                          var errorFields = response['message'].keys;
+                          for (var errorField in errorFields) {
+                            responseErrors.putIfAbsent(errorField,
+                                () => response['message'][errorField]);
+                          }
+                          setState(() {
+                            errors = responseErrors;
+                          });
+                        }
+                      });
                     } else {
-                      Map<String, dynamic> responseErrors = {};
-                      var errorFields = response['message'].keys;
-                      for (var errorField in errorFields) {
-                        responseErrors.putIfAbsent(
-                            errorField, () => response['message'][errorField]);
-                      }
                       setState(() {
-                        errors = responseErrors;
+                        _autovalidate = true;
                       });
                     }
-                  });
-                } else {
-                  setState(() {
-                    _autovalidate = true;
-                  });
-                }
-              },
-              child: Text(
-                "Register",
-                style: TextStyle(color: Colors.white),
-              ),
-              color: Theme.of(context).primaryColor,
-            ),
-          ],
-        ),);
+                  },
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: Theme.of(context).primaryColor,
+                ),
+        ],
+      ),
+    );
   }
 }
