95050b39a556a415035376271bdf1bbeb365f445
Chege Bryan <cbbryan8@gmail.com>
2021-06-16 14:58:07 +0300

    Add screentshots on answer

--- lib/widgets/question_answer_widget.dart
+++ lib/widgets/question_answer_widget.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 class QuestionAnswerWidget extends StatefulWidget {
   final String question;
   final String answer;
-  final List<String> images;
+  final List<String> screenshots;
 
   const QuestionAnswerWidget({
     Key key,
     @required this.question,
     @required this.answer,
-    this.images,
+    this.screenshots,
   }) : super(key: key);
 
   @override
@@ -31,6 +31,8 @@ class _QuestionAnswerWidgetState extends State<QuestionAnswerWidget> {
 
   @override
   Widget build(BuildContext context) {
+    print(widget.screenshots);
+
     return Container(
       decoration: BoxDecoration(
         color: const Color(0xFFF5F4F4),
@@ -75,13 +77,23 @@ class _QuestionAnswerWidgetState extends State<QuestionAnswerWidget> {
             visible: _answerVisible,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text(
-                widget.answer,
-                style: TextStyle(
-                  fontFamily: 'Avenir',
-                  fontSize: 16.0,
-                  height: 1.5,
-                ),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    widget.answer,
+                    style: TextStyle(
+                      fontFamily: 'Avenir',
+                      fontSize: 16.0,
+                      height: 1.5,
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      for (var screenshot in widget.screenshots)
+                        Image.asset('assets/$screenshot')
+                    ],
+                  )
+                ],
               ),
             ),
           ),
