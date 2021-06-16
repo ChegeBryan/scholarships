43b59fb9494b023f78b28140d73033b038d14c04
Peter Kingori <kpndegwa@gmail.com>
2021-06-16 10:45:22 +0300

    Add navigation for faq page

--- lib/faq_screen.dart
+++ lib/faq_screen.dart
@@ -10,11 +10,21 @@ class FaqScreen extends StatefulWidget {
 class _FaqScreenState extends State<FaqScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
+    return SafeArea(
+      child: Scaffold(
+        drawer: AppDrawer(),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Text(
+                'Frequently Asked Questions:',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
       ),
-      drawer: AppDrawer(),
     );
   }
 }
