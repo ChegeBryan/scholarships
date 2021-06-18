c346042b083c45831814641ef1c42a724d4643db
Peter Kingori <kpndegwa@gmail.com>
2021-06-18 10:14:19 +0300

    Make each category to be selected individually

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 
 class CategoryCard extends StatefulWidget {
   final String categoryName;
+  final int index;
+  final bool isSelected;
+  final VoidCallback onSelect;
 
-  CategoryCard({this.categoryName});
+  CategoryCard({this.categoryName, this.index, this.isSelected, this.onSelect});
 
   @override
   _CategoryCardState createState() => _CategoryCardState();
@@ -18,26 +21,22 @@ class _CategoryCardState extends State<CategoryCard> {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 6.0),
       child: FlatButton(
-        color: _isPressed ? Color(0xff1E22AA) : null,
+        onPressed: widget.onSelect,
+        color: widget.isSelected ? Color(0xff1E22AA) : Colors.transparent,
         padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 1.0),
         child: Text(
           widget.categoryName,
           style: TextStyle(
-              color: _isPressed ? Colors.white : Color(0xff3A5160),
+              color: widget.isSelected ? Colors.white : Color(0xff3A5160),
               fontWeight: FontWeight.bold),
         ),
-        shape: _isPressed
+        shape: widget.isSelected
             ? RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(50),
                 side: BorderSide(color: Colors.transparent))
             : RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(50),
                 side: BorderSide(color: Color(0xff9E9E9E))),
-        onPressed: () {
-          setState(() {
-            _isPressed = !_isPressed;
-          });
-        },
       ),
     );
   }
