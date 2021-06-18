280eca3a5e6969ce454013f4fa27343737d5eb0d
Chege Bryan <cbbryan8@gmail.com>
2021-06-18 14:05:47 +0300

    Add convert to json method faqs

--- lib/models/faq.dart
+++ lib/models/faq.dart
@@ -1,10 +1,14 @@
-import 'package:geopro/models/faq_category.dart';
-
-class Faq extends FaqCategory {
+class Faq {
   final String question;
   final String answer;
   final List<String> images;
+  final String category;
 
-  Faq({this.question, this.answer, this.images, String category})
-      : super(category: category);
+  Faq({this.question, this.answer, this.images, this.category});
+
+  Faq.fromJson(Map<String, dynamic> json)
+      : question = json['question'],
+        answer = json['answer'],
+        images = json['images'],
+        category = json['category'];
 }
