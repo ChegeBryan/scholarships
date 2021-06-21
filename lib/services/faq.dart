2d334f94b8cbbbc113a90b75ef20ef9857dbf12c
Chege Bryan <cbbryan8@gmail.com>
2021-06-21 15:10:34 +0300

    Remove duplicate question

--- lib/services/faq.dart
+++ lib/services/faq.dart
@@ -52,16 +52,6 @@ List faqs = [
     "category": "Safety"
   },
   {
-    "question": "How to upload PPE and SSEs?",
-    "answer":
-        "Tortor pretium viverra suspendisse potenti nullam ac. Convallis aenean et tortor at risus viverra. Augue ut lectus arcu bibendum at varius vel pharetra. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Tempus iaculis urna id volutpat lacus laoreet. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Euismod elementum nisi quis eleifend quam. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Nulla facilisi morbi tempus iaculis. Dictum fusce ut placerat orci nulla pellentesque dignissim. Mattis aliquam faucibus purus in massa. Et malesuada fames ac turpis egestas maecenas pharetra convallis. Ipsum nunc aliquet bibendum enim facilisis gravida.",
-    "images": [
-      "https://placeimg.com/640/480/tech",
-      "https://placeimg.com/640/480/tech"
-    ],
-    "category": "Safety"
-  },
-  {
     "question": "How to upload certificates?",
     "answer":
         "Tortor pretium viverra suspendisse potenti nullam ac. Convallis aenean et tortor at risus viverra. Augue ut lectus arcu bibendum at varius vel pharetra. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Tempus iaculis urna id volutpat lacus laoreet. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Euismod elementum nisi quis eleifend quam. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit. Nulla facilisi morbi tempus iaculis. Dictum fusce ut placerat orci nulla pellentesque dignissim. Mattis aliquam faucibus purus in massa. Et malesuada fames ac turpis egestas maecenas pharetra convallis. Ipsum nunc aliquet bibendum enim facilisis gravida.",
