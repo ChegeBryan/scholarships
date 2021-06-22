b0cd702495082c4bd3cbe2500a5f71f17cc85568
Chege Bryan <cbbryan8@gmail.com>
2021-06-22 13:53:39 +0300

    Add custom sliver appbar

---
+++ lib/widgets/appbar_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends StatelessWidget {
+  const AppBarWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: MediaQuery.of(context).size.height * 0.3,
+      stretch: true,
+      pinned: true,
+      elevation: 0,
+      flexibleSpace: FlexibleSpaceBar(
+        stretchModes: const <StretchMode>[
+          StretchMode.zoomBackground,
+          StretchMode.blurBackground,
+          StretchMode.fadeTitle,
+        ],
+        title: Text(
+          'Sponsorships',
+        ),
+        centerTitle: true,
+        background: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image.asset(
+              'assets/student_graduate.jpg',
+              fit: BoxFit.cover,
+            ),
+            const DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment(0.0, 0.5),
+                  end: Alignment.center,
+                  colors: <Color>[
+                    Color(0x60000000),
+                    Color(0x00000000),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
