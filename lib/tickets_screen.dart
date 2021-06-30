80fa89c1afbd09398ac7f00b3c6a9d7f34c80472
Chege Bryan <cbbryan8@gmail.com>
2021-06-30 19:50:30 +0300

    Add tickets filter fields

--- lib/tickets_screen.dart
+++ lib/tickets_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:geopro/widgets/app_drawer.dart';
 import 'package:intl/intl.dart';
+
+import 'package:date_range_picker/date_range_picker.dart' as DateRangePicker;
 
 class TicketsScreen extends StatefulWidget {
   const TicketsScreen({Key key}) : super(key: key);
@@ -63,9 +67,11 @@ class _TicketsScreenState extends State<TicketsScreen> {
                     color: Color(0xFF3A5160),
                   ),
                 ),
-                titlePadding: const EdgeInsetsDirectional.only(start: 16),
+                titlePadding: const EdgeInsetsDirectional.only(start: 20),
               ),
+              //backgroundColor: Colors.blue,
             ),
+            SliverFiltersHeader(),
             SliverFillRemaining(
               child: Center(
                 child: Text('Tickets go here.'),
@@ -75,5 +81,147 @@ class _TicketsScreenState extends State<TicketsScreen> {
         ),
       ),
     );
+  }
+}
+
+class SliverFiltersHeader extends StatelessWidget {
+  SliverFiltersHeader({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: SliverAppBarDelegate(
+        minHeight: 136.0,
+        maxHeight: 136.0,
+        child: Container(
+          padding: const EdgeInsets.all(12.0),
+          // color: Colors.cyan,
+          //height: 48.0,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Material(
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0)),
+                  child: SizedBox(
+                    height: 48.0,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Theme.of(context).textTheme.body1.color,
+                        ),
+                        hintText: 'Technician or Ticket Number',
+                        hintStyle: TextStyle(
+                          color: Color(0xFF9E9E9E),
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: DateRangePickerButton(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DateRangePickerButton extends StatefulWidget {
+  const DateRangePickerButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _DateRangePickerButtonState createState() => _DateRangePickerButtonState();
+}
+
+class _DateRangePickerButtonState extends State<DateRangePickerButton> {
+  List<DateTime> dateRange = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              dateRange.length == 0
+                  ? 'Select Date range'
+                  : dateRange.length == 1
+                      ? DateFormat.yMMMd().format(dateRange[0])
+                      : (DateFormat.yMMMd().format(dateRange[0]) +
+                          ' - ' +
+                          DateFormat.yMMMd().format(dateRange[1])),
+            ),
+          ),
+          Icon(Icons.chevron_right)
+        ],
+      ),
+      onPressed: () async {
+        final List<DateTime> picked = await DateRangePicker.showDatePicker(
+          context: context,
+          initialFirstDate: dateRange.length != 0
+              ? dateRange[0]
+              : (DateTime.now()).subtract(Duration(days: 7)),
+          initialLastDate: dateRange.length != 0 && dateRange.length == 2
+              ? dateRange[1]
+              : dateRange.length == 1 ? dateRange[0] : DateTime.now(),
+          firstDate: new DateTime(2020),
+          lastDate: new DateTime.now(),
+        );
+
+        if (picked != null) {
+          setState(() {
+            dateRange = picked;
+          });
+        }
+      },
+    );
+  }
+}
+
+class SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  SliverAppBarDelegate({
+    @required this.minHeight,
+    @required this.maxHeight,
+    @required this.child,
+  });
+
+  @override
+  double get minExtent => minHeight;
+
+  @override
+  double get maxExtent => max(maxHeight, minHeight);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
   }
 }
