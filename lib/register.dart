5fb23ada5f30e86745de927d79bf626ad87aa72d
Chege Bryan <cbbryan8@gmail.com>
2021-07-01 16:38:01 +0300

    Add background register screen

--- lib/register.dart
+++ lib/register.dart
@@ -12,29 +12,40 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Color(0xFF5464BF), Theme.of(context).primaryColor],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter),
-        ),
-        height: MediaQuery.of(context).size.height,
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            RegisterForm(),
-            FormBottomActions(
-              message: 'Have account?',
-              action: () {
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              actionText: 'Login',
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xFF16202B),
             ),
-          ],
-        ),
+          ),
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Image.asset(
+              'assets/student_graduate.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                RegisterForm(),
+                FormBottomActions(
+                  message: 'Have account?',
+                  action: () {
+                    Navigator.pushReplacementNamed(context, '/login');
+                  },
+                  actionText: 'Login',
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
