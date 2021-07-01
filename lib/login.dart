413e4677e7dbfa0adadc2dfd6d9a4d6e4ab5cbab
Chege Bryan <cbbryan8@gmail.com>
2021-07-01 14:33:26 +0300

    Update login screen with background image

--- lib/login.dart
+++ lib/login.dart
@@ -11,28 +11,22 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Theme.of(context).primaryColor, Color(0xFF5464BF)],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter),
-        ),
-        height: MediaQuery.of(context).size.height,
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            LoginForm(),
-            FormBottomActions(
-              message: 'Don\'t have account?',
-              action: () {
-                Navigator.pushReplacementNamed(context, '/register');
-              },
-              actionText: 'Register',
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xFF16202B),
             ),
-          ],
-        ),
+          ),
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Image.asset(
+              'assets/student_graduate.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
       ),
     );
   }
